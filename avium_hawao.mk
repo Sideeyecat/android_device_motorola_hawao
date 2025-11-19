5476041b1325cf83bfc96e5a42bdddc481fbbe48
Sideeyecat <sohamasme3@gmail.com>
2025-11-19 20:09:56 +0530

    Create avium_hawao.mk

---
+++ avium_hawao.mk
@@ -0,0 +1,17 @@
+#
+# SPDX-FileCopyrightText: 2025 The AviumUI Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+AVIUM_BUILDTYPE = Unofficial
+AVIUM_VERSION_APPEND_TIME_OF_DAY = false
+
+# GMS
+WITH_GMS := true
+TARGET_GMS_TYPE := FULL
+
+# LatinIMEGooglePrebuilt
+TARGET_INCLUDE_GOOGLEIME := true
+TARGET_GOOGLEIME_OVERRIDE_IME := true
+
+AVIUM_MAINTAINER := helloisnotadevsadly
