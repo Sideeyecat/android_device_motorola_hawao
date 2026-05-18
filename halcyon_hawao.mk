c3734857f1f6e31b4c5dc02f1a463eacfbf38874
Sideeyecat <sohamasme3@gmail.com>
2026-05-18 09:38:23 +0530

    Fix halcyon_hawao.mk

--- halcyon_hawao.mk
+++ halcyon_hawao.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/hawao/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/halcyon/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := halcyon_hawao
